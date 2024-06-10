170dee0b94f40866acef837140ec2d25677d1f68
Edilson Junior <edilsonrsjunior@gmail.com>
2024-06-10 19:20:04 -0400

    cors and listening all network ports for development purpose

--- src/Fiap.WebAPI/Program.cs
+++ src/Fiap.WebAPI/Program.cs
@@ -1,23 +1,35 @@
 using Fiap.Application.IoC;
 using Fiap.Data.Context;
 using Microsoft.EntityFrameworkCore;
-using System.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-builder.Services.AddDbContext<FiapContext>
-    (options => options.UseOracle
-    (builder.Configuration.GetConnectionString("DefaultConnection")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<FiapContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddResolveDependencies();
+
+// Configurar CORS para permitir todas as origens (ou configure conforme suas necessidades)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -28,9 +40,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+app.UseCors("AllowAll");
+
+// Configure URL para escutar em todas as interfaces de rede na porta 5152
+app.Urls.Add("http://*:5152");
 
 app.MapControllers();
 
